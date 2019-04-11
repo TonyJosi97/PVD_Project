44834f1246b726a562a9cd2715cfdd045cd0af13
Tony Josi <tonyjosi@Tonys-MacBook-Air.local>
2019-04-11 14:17:14 +0530

    Commented the extraction file and formatted

--- pvdExtract.py
+++ pvdExtract.py
@@ -1,52 +1,74 @@
 from PIL import Image
 import sys, os
 
+# File Objects creation
 im = Image.open(sys.argv[1])
-# input = open(sys.argv[2], "w")
+outp = open(sys.argv[2], "w")
 lg = open("embedlog.log", "r")
 
+# Initialisation
 pix = im.load()
-hi, wi = im.size
 temp = 1
 chrtr = ""
 
-while True:
-    st = lg.readline()
-    if len(st) == 0:
-        print(chr(int(chrtr, 2)))
-        break
-    i, j, pixel, diff, pad, charNum = st.split()
-    i = int(i)
-    j = int(j)
-    diff = int(diff)
-    pad = int(pad)
-    charNum = int(charNum)
-    # print(i, j, pixel, diff, pad, charNum)
-    # print(pix[i,j])
-    r, g, b = pix[i, j]
-    if temp != charNum:
-        print(chr(int(chrtr, 2)), end="")
-        # print()
-        chrtr = ""
-    if pixel == "r":
-        binr = bin(r)
-        chrtr += binr[(len(binr) - diff) :]
-        # print(i,j,pixel,binr,"before pad - ",chrtr,end=" ")
+# Main Function
+def main():
+    global chrtr, temp
+    while True:
+
+        # Read each line from log file
+        st = lg.readline()
+
+        # Check if log file reached its end
+        if len(st) == 0:
+            # Write extracted data to file
+            print(chr(int(chrtr, 2)))
+            outp.write(chr(int(chrtr, 2)))
+            break
+
+        # Unpack line read from log file to variables
+        i, j, pixel, diff, pad, charNum = st.split()
+
+        # Process variables
+        i = int(i)
+        j = int(j)
+        diff = int(diff)
+        pad = int(pad)
+        charNum = int(charNum)
+        r, g, b = pix[i, j]
+
+        # Check if a new character in embed log is reached
+        if temp != charNum:
+            print(chr(int(chrtr, 2)), end="")
+            outp.write(chr(int(chrtr, 2)))
+            chrtr = ""
+
+        # If embedded pixel is red
+        if pixel == "r":
+            binr = bin(r)
+            chrtr += binr[(len(binr) - diff) :]
+
+        # If embedded pixel is green
+        if pixel == "g":
+            binr = bin(g)
+            chrtr += binr[(len(binr) - diff) :]
+
+        # If embedded pixel is blue
+        if pixel == "b":
+            binr = bin(b)
+            chrtr += binr[(len(binr) - diff) :]
+
+        # Unpad if padding is done
         if pad != 0:
             chrtr = chrtr[: (len(chrtr) - pad)]
-            # print("after pad - ",chrtr,end=" ")
-    if pixel == "g":
-        binr = bin(g)
-        chrtr += binr[(len(binr) - diff) :]
-        # print("before pad - ",chrtr,end=" ")
-        if pad != 0:
-            chrtr = chrtr[: (len(chrtr) - pad)]
-            # print("after pad - ",chrtr,end=" ")
-    if pixel == "b":
-        binr = bin(b)
-        chrtr += binr[(len(binr) - diff) :]
-        # print("before pad - ",chrtr,end=" ")
-        if pad != 0:
-            chrtr = chrtr[: (len(chrtr) - pad)]
-            # print("after pad - ",chrtr,end=" ")
-    temp = charNum
+
+        # For checking if character has changed in embed file
+        temp = charNum
+
+    # Close file objects
+    outp.close()
+    lg.close()
+
+
+if __name__ == "__main__":
+    main()
